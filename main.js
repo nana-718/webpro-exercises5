36c06e2cbf386a992c6840724ff19760b59a6f25
nana-718 <75443953+nana-718@users.noreply.github.com>
2021-01-31 23:47:22 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -8,28 +8,21 @@ function randomValueFromArray(array){
 
 let storyText = 'It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weights 300 pounds, and it was a hot day.'
 
-let insertX = ['Willy the Goblin',
-              'Big Daddy',
-              'Father Christmas'];
+let insertX = ['Willy the Goblin','Big Daddy', 'Father Christmas'];
 
-let insertY = ['the soup kitchen',
-              'Disneyland',
-              'the White House'];
+let insertY = ['the soup kitchen','Disneyland','the White House'];
 
-let insertZ = ['spontaneously combusted',
-              'melted into a puddle on the sidewalk',
-              'turned into a slug and crawled away'];
-
+let insertZ = ['spontaneously combusted', 'melted into a puddle on the sidewalk', 'turned into a slug and crawled away'];
 
 randomize.addEventListener('click', result);
 
-
 function result() {
-
-  let newStory = storyText;
   let xItem = randomValueFromArray(insertX);
   let yItem = randomValueFromArray(insertY);
   let zItem = randomValueFromArray(insertZ);
+
+  let newStory = storyText;
+
   newStory = newStory.replace(/:insertx:/g, randomValueFromArray(insertX));
   newStory = newStory.replace(':inserty:', randomValueFromArray(insertY));
   newStory = newStory.replace(':insertz:', randomValueFromArray(insertZ));
@@ -46,7 +39,6 @@ function result() {
     newStory = newStory.replace('94 fahrenheit', Math.round((94 - 32) / 1.800000) + ' centigrade');
   }
   
-
   story.textContent = newStory;
   story.style.visibility = 'visible';
 }
